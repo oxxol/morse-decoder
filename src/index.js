cedfd00b0efcc220f758d0f2f98e5481c00e5075
Oxana Oleynikova <oleynikova.oxana@gmail.com>
2022-02-01 04:46:45 +0100

    complete task

--- src/index.js
+++ src/index.js
@@ -38,7 +38,55 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let arrayNumbers = [];
+let key = '';
+let arrayKeys = [];
+    for (i = 0; i < expr.length; i = i + 10) {
+        arrayNumbers.push(expr.substring(i, i + 10))
+  
+};
+console.log(arrayNumbers.length)
+arrayNumbers.forEach((el) => {
+        if (el === '**********') {
+            key = ' ';
+            arrayKeys.push(key)
+        }
+        else {
+            for (let j = 0; j < 10; j = j + 2) {
+                
+                if (el.substring(j, (j + 2)) === '00') {
+                    key = key + '';
+                }
+                    else if (el.substring(j, j + 2) === '11') {
+                    key = key + '-';
+                }
+                    else if (el.substring(j, j + 2) === '10') {
+                    key = key + '.';
+                };
+            } arrayKeys.push(key)
+            
+    }key = '';
+    return arrayKeys;
+    } 
+    );
+    let string = '';
+    let letter = '';
+arrayKeys.forEach(function (el) {
+        if (el == ' ') {
+            letter = ' ';
+        }
+        else {
+            letter = MORSE_TABLE[el];
+            
+        }
+    string = string + letter;  
+    letter = '';
+        
+        return string;
+        
+})
+    return string;
+
 }
 
 module.exports = {
